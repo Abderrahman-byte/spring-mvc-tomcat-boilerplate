87ccc81c3f901335e26856139c360e1c9042f0aa
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-12 11:24:07 +0100

    Initialize maven quickapp

---
+++ src/test/java/com/abderrahmane/webappexec/AppTest.java
@@ -0,0 +1,20 @@
+package com.abderrahmane.webappexec;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
